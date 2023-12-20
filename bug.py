de8727e6456bb9390cce79fc5b1a38d61af5695a
cct <572879152@qq.com>
2023-12-20 11:47:47 +0800

    优化了招呼语，某某总你好！这样不会有群发的嫌疑，还舔了HR，HR应该挺舒服的。

--- bug.py
+++ bug.py
@@ -5,8 +5,8 @@ import pandas as pd
 import code_mapping_city
 from fake_useragent import UserAgent
 from urllib.parse import urlparse, urlunparse
-import csv
 from Models.Jobs import Jobs
+from selenium.webdriver.common.keys import Keys
 
 ua = UserAgent()
 u_a = ua.random
@@ -14,37 +14,45 @@ chrome_op = webdriver.ChromeOptions()
 chrome_op.add_argument(f'user-agent={u_a}')
 driver = webdriver.Chrome(options=chrome_op)
 
-query_job = 'web'  # 查什么工作
-experience = '102,101'  # 经验要求102=应届，103=一年以内，101=不限
+query_job = 'python'  # 查什么工作
+experience = '102,101,103'  # 经验要求102=应届，103=一年以内，101=不限
 salary = '405,404'  # 405 10-20k,404 5-10k
 query_city = code_mapping_city.get_city_code('杭州')  # 查找的城市代码
 # query_area = code_mapping_city.get_region_code('余杭区') #查找的区域代码
-query_areas = ['上城区', '余杭区', '拱墅区', '西湖区', '滨江区', '萧山', '钱塘区']
+query_areas = ['上城区', '余杭区', '拱墅区', '西湖区', '滨江区', '萧山', '钱塘区', '临平区']
 
 # query_areas = [code_mapping_city.get_region_code(area) for area in query_areas]
 
 
 def login():
-    driver.get('https://www.zhipin.com/web/user/?ka=header-login')
-    login = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[3]/button')
-    confirm = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[2]/span/input')
-    send_code = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div/span/div')
-    tel = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/span[2]/input')
+    login_mode = input('选择登录方式(验证码或扫码)，验证码输入1扫码输入2')
+    if login_mode == '1':
+        driver.get('https://www.zhipin.com/web/user/?ka=header-login')
+        login = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[3]/button')
+        confirm = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[2]/span/input')
+        send_code = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div/span/div')
+        tel = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/span[2]/input')
 
-    tel.send_keys(18058059037)
-    confirm.click()
-    send_code.click()
-    time.sleep(5)
-    hascode = True
+        tel.send_keys(13735784705)
+        confirm.click()
+        send_code.click()
+        time.sleep(5)
+        hascode = True
 
-    while hascode:
-        code = driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div/span/input')
-        code = code.get_attribute('value')
-        if str(code) != '' and len(str(code)) >= 6:
-            hascode = False
-            print('登录成功')
-    login.click()
-    time.sleep(5)
+        while hascode:
+            code = driver.find_element(By.XPATH,
+                                       '//*[@id="wrap"]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div/span/input')
+            code = code.get_attribute('value')
+            if str(code) != '' and len(str(code)) >= 6:
+                hascode = False
+                print('登录成功')
+        login.click()
+        time.sleep(5)
+    elif login_mode == '2':
+        driver.get('https://www.zhipin.com/web/user/?ka=header-login')
+        time.sleep(15)
+
+
 
 
 def bugInfo():
@@ -126,7 +134,7 @@ def bugInfo():
 def bugUrl():
     push_count = 0
     query = (Jobs.select(Jobs.url, Jobs.chated, Jobs.id_job, Jobs.job_name)
-             .where(((Jobs.job_name.contains('web')) | Jobs.job_name.contains('vue') | Jobs.job_name.contains('前端') | Jobs.job_name.contains('vue3') | (Jobs.job_name.contains('Web'))) & (Jobs.active != 0) & (Jobs.chated != 0)))
+             .where(((Jobs.job_name.contains('数据分析师')) | Jobs.job_name.contains('python') | Jobs.job_name.contains('爬虫') | Jobs.job_name.contains('测试') | (Jobs.job_name.contains('后端开发'))) & (Jobs.active == 1) & (Jobs.chated == 0)))
     query = query.execute()
     for i in query:
         if push_count == 100:
@@ -154,14 +162,42 @@ def bugUrl():
         try:
             driver.find_element(By.CLASS_NAME, 'boss-active-time')
         except Exception:
+
+            try:
+                driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/div/div/div[1]/div[3]/div[1]/a[2]')
+            except Exception:
+                continue
+            chat = driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/div/div/div[1]/div[3]/div[1]/a[2]')
+            chat.click()
+            driver.implicitly_wait(5)
+            name = driver.find_element(By.XPATH, '//*[@id="container"]/div/div/div[2]/div[1]/div[1]/p/span[1]').text
+            first_name = name[0]
+            chat_input = driver.find_element(By.CLASS_NAME, 'chat-input')
+            chat_input.send_keys((first_name+'总你好！我对这个岗位很感兴趣，方便聊聊吗？'), Keys.ENTER)
+            print('已经沟通')
+            Jobs.update({'chated': 1}).where(Jobs.id_job == i.id_job).execute()
+            push_count = push_count + 1
             continue
+
         active_time = driver.find_element(By.CLASS_NAME, 'boss-active-time')
+
         # chated = Jobs.get_by_id(Jobs.id_job==i.id_job)
         # print('chated=' + str(chated.chated))
         if active_time.get_attribute('textContent') in ['刚刚活跃', '本周活跃', '3日内活跃', '4日内活跃', '2日内活跃'
-                                                        '今日活跃', '在线']:
+                                                        '今日活跃']:
+            try:
+                driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/div/div/div[1]/div[3]/div[1]/a[2]')
+            except Exception:
+                continue
             chat = driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/div/div/div[1]/div[3]/div[1]/a[2]')
             chat.click()
+
+            driver.implicitly_wait(5)
+            name = driver.find_element(By.XPATH, '//*[@id="container"]/div/div/div[2]/div[1]/div[1]/p/span[1]').text
+            first_name = name[0]
+            chat_input = driver.find_element(By.CLASS_NAME, 'chat-input')
+            chat_input.send_keys((first_name + '总你好！我对这个岗位很感兴趣，方便聊聊吗？'), Keys.ENTER)
+
             print('已经沟通')
             Jobs.update({'chated': 1}).where(Jobs.id_job == i.id_job).execute()
             push_count = push_count + 1
